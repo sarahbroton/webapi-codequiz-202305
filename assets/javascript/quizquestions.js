5b117bb1df824a09842f8fb7734ef8bcfc8b8917
sarahbroton <sarah.broton@gmail.com>
2023-05-20 22:24:44 -0500

    addquizquestionjs-update11

---
+++ assets/javascript/quizquestions.js
@@ -0,0 +1,29 @@
+var questions = [
+
+{
+
+    question: "Which is NOT a JS data type?"
+    choices: ['pickles', 'strings', 'booleans', 'integers'], 
+    answer: 'pickles'
+}
+
+{
+
+    question: "question?"
+    choices: ['item1', 'item2', 'item3', 'item4'], 
+    answer: 'cheese'
+}
+
+{
+
+    question: "question?"
+    choices: ['item1', 'item2', 'item3', 'item4'], 
+    answer: 'cheese'
+}
+
+
+]
+
+questions[currentquestion].question
+questions[currentquestion].choices
+questions[currentquestion].answer
